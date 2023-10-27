978d323abcaf49c49d7357e5a320a41656c1c190
naremare <nare.sarksyan@gmail.com>
2023-10-27 09:21:21 +0200

    base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "my-bank/",
     build: {
         rollupOptions: {
             input: {
